56a4e17eb5702e6ecbe3ef2da02422c290345005
LUCAS VINICIUS LOPES DO NASCIMENTO <16254859@CFP127ALUNO.LOCAL>
2017-11-08 10:04:56 -0200

    primeiro comit

---
+++ app/src/main/java/br/com/testequilometragem/app/MainActivity.java
@@ -0,0 +1,47 @@
+package br.com.testequilometragem.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity{
+
+    private Spinner categoria;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.categoria_gasto,
+                android.R.layout.simple_spinner_item);
+        categoria = (Spinner) findViewById(R.id.meses);
+        categoria.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.valores) {
+            startActivity(new Intent(this, Valores.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/br/com/testequilometragem/app/Valores.java
@@ -0,0 +1,14 @@
+package br.com.testequilometragem.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Valores extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.valores);
+    }
+}
